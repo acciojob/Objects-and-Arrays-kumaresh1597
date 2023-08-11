2ea4a9d1140b713fb10becc254f08ca4712f80f7
Kumaresh <kumaresh.1597@gmail.com>
2023-08-11 09:12:07 +0530

    created a submission

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ const person = {
 // Write your code here, make sure the name of variables is exactly same as mentioned in the problem statement
 let team = players;
 let team1 =[];
-for (let index = 0; index < player.length; index++) {
+for (let index = 0; index < players.length; index++) {
 	team1[index] = team[index];
 }
 
